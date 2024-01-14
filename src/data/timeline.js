54da547e8ae32180e1ce9f4864e3567efd349615
Alex Stolzman <47224184+alexstolzman@users.noreply.github.com>
2024-01-13 19:10:00 -0600

    Update timeline.js

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -12,8 +12,13 @@ export default [
         details: "Attended the University of Wisconsin Stout and graduated in December 2021."
     },
     {
-        year: "2022-Current",
+        year: "2022-July 2023",
         title: "Independent Developer",
         details: "Took a break after graduating. Working on independent projects and finding a job."
+    },
+    {
+        year: "July 2023-Current",
+        title: "Software Developer",
+        details: "Software Developer for Ardisam, Inc."
     }
 ]
